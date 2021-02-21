1b2cf67f78bc7eab351ac1991e5e52dfe1efa7a8
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-21 16:31:32 +0100

    Stage 3/6 of the project completed! - now the program prints the lines and counts the number of stops that each line has

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -3,31 +3,38 @@ import re
 
 json_input_string = input()
 input_list = json.loads(json_input_string)
-errors_list = [0, 0, 0, 0]
+lineNames = []
+numberOfStops = [0, 0, 0, 0]
 
 
 def checkData(json_string):
-    global errors_list
+    global lineNames
+    global numberOfStops
     for dic in json_string:
-        if not re.match(r'[A-Z][a-z]+\s?[A-Z]?[a-z]+?\s(Avenue|Street|Road|Boulevard)$', dic['stop_name']):
-            errors_list[1] += 1
-            errors_list[0] += 1
-        if dic['stop_type'] not in ['S', 'O', 'F', '']:
-            errors_list[2] += 1
-            errors_list[0] += 1
-        if not re.match(r'[0-2][0-9]:[0-5][0-9]$', dic['a_time']):
-            errors_list[3] += 1
-            errors_list[0] += 1
+        if dic['bus_id'] == 128:
+            numberOfStops[0] += 1
+            if dic['bus_id'] not in lineNames:
+                lineNames.append(dic['bus_id'])
+        elif dic['bus_id'] == 256:
+            numberOfStops[1] += 1
+            if dic['bus_id'] not in lineNames:
+                lineNames.append(dic['bus_id'])
+        elif dic['bus_id'] == 512:
+            numberOfStops[2] += 1
+            if dic['bus_id'] not in lineNames:
+                lineNames.append(dic['bus_id'])
+        else:
+            numberOfStops[3] += 1
+            if dic['bus_id'] not in lineNames:
+                lineNames.append(dic['bus_id'])
 
 
 def printErrorDictionary():
-    global errors_list
-    error_dictionary = {'Format validation': f'{errors_list[0]} errors',
-                        'stop_name': errors_list[1],
-                        'stop_type': errors_list[2],
-                        'a_time': errors_list[3]}
-    for key, value in error_dictionary.items():
-        print(key, ': ', value)
+    global lineNames
+    global numberOfStops
+    print("Line names and number of stops:")
+    for index in range(len(lineNames)):
+        print("bus_id:", lineNames[index], ", stops:", numberOfStops[index])
 
 
 def startProgram():
