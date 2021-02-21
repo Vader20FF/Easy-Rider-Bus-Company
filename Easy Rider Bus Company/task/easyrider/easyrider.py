7ca41df3426eaa6b3f4270ae7b6fd983f765afed
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-21 16:15:11 +0100

    Stage 2/6 of the project completed! - now the program uses regular expressions to check whether certain fields in json string are in valid format

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,43 +1,31 @@
 import json
+import re
 
-# Write your awesome code here
 json_input_string = input()
 input_list = json.loads(json_input_string)
-errors_list = [0, 0, 0, 0, 0, 0, 0]
+errors_list = [0, 0, 0, 0]
 
 
 def checkData(json_string):
     global errors_list
     for dic in json_string:
-        if not isinstance(dic['bus_id'], int) or not str(dic['bus_id']):
+        if not re.match(r'[A-Z][a-z]+\s?[A-Z]?[a-z]+?\s(Avenue|Street|Road|Boulevard)$', dic['stop_name']):
             errors_list[1] += 1
             errors_list[0] += 1
-        if not isinstance(dic['stop_id'], int) or not str(dic['stop_id']):
+        if dic['stop_type'] not in ['S', 'O', 'F', '']:
             errors_list[2] += 1
             errors_list[0] += 1
-        if not isinstance(dic['stop_name'], str) or not str(dic['stop_name']):
+        if not re.match(r'[0-2][0-9]:[0-5][0-9]$', dic['a_time']):
             errors_list[3] += 1
-            errors_list[0] += 1
-        if not isinstance(dic['next_stop'], int) or not str(dic['next_stop']):
-            errors_list[4] += 1
-            errors_list[0] += 1
-        if dic['stop_type'] not in ('S', 'O', 'F', ''):
-            errors_list[5] += 1
-            errors_list[0] += 1
-        if not isinstance(dic['a_time'], str) or len(dic['a_time']) != 5:
-            errors_list[6] += 1
             errors_list[0] += 1
 
 
 def printErrorDictionary():
     global errors_list
-    error_dictionary = {'Type and required field validation': f'{errors_list[0]} errors',
-                        'bus_id': errors_list[1],
-                        'stop_id': errors_list[2],
-                        'stop_name': errors_list[3],
-                        'next_stop': errors_list[4],
-                        'stop_type': errors_list[5],
-                        'a_time': errors_list[6]}
+    error_dictionary = {'Format validation': f'{errors_list[0]} errors',
+                        'stop_name': errors_list[1],
+                        'stop_type': errors_list[2],
+                        'a_time': errors_list[3]}
     for key, value in error_dictionary.items():
         print(key, ': ', value)
 
@@ -48,5 +36,3 @@ def startProgram():
 
 
 startProgram()
-
-
