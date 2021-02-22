4c66f42e703cd887832ea411e3f87875ff1e998c
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-22 19:28:55 +0100

    Stage 4/6 of the project completed! - this stage of the project was about finding if every line has it beginning and the end. Also a number of transfer stops was counted. A transfer stop is a stop where at least two different bus lines have their stop

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,45 +1,76 @@
 import json
-import re
+import sys
 
 json_input_string = input()
 input_list = json.loads(json_input_string)
-lineNames = []
-numberOfStops = [0, 0, 0, 0]
+startStops = set()
+transferStops = set()
+finishStops = set()
 
 
 def checkData(json_string):
-    global lineNames
-    global numberOfStops
+    stops = {128: {'start_stop': False, 'finish_stop': False}, 256: {'start_stop': False, 'finish_stop': False},
+             512: {'start_stop': False, 'finish_stop': False}}
+    transferStopsCounter = {"Sesame Street": 0, "Fifth Avenue": 0, "Prospekt Avenue": 0, "Elm Street": 0,
+                            "Sunset Boulevard": 0, "Bourbon Street": 0, "Pilotow Street": 0, "Abbey Road": 0,
+                            "Santa Monica Boulevard": 0, "Beale Street": 0, "Startowa Street": 0, "Lombard Street": 0,
+                            "Orchard Road": 0, "Khao San Road": 0, "Michigan Avenue": 0, "Arlington Road": 0,
+                            "Niebajka Avenue": 0, "Jakis Street": 0, "Jakas Avenue": 0, "Karlikowska Avenue": 0,
+                            "Parizska Street": 0}
+    currentBusLines = set()
+    global startStops, transferStops, finishStops
     for dic in json_string:
-        if dic['bus_id'] == 128:
-            numberOfStops[0] += 1
-            if dic['bus_id'] not in lineNames:
-                lineNames.append(dic['bus_id'])
-        elif dic['bus_id'] == 256:
-            numberOfStops[1] += 1
-            if dic['bus_id'] not in lineNames:
-                lineNames.append(dic['bus_id'])
-        elif dic['bus_id'] == 512:
-            numberOfStops[2] += 1
-            if dic['bus_id'] not in lineNames:
-                lineNames.append(dic['bus_id'])
-        else:
-            numberOfStops[3] += 1
-            if dic['bus_id'] not in lineNames:
-                lineNames.append(dic['bus_id'])
+        # Checking transfer stops
+        if dic['stop_name'] in transferStopsCounter:
+            transferStopsCounter[dic['stop_name']] += 1
+        if transferStopsCounter[dic['stop_name']] > 1:
+            transferStops.add(dic['stop_name'])
+
+        # Checking start stops
+        if dic['stop_type'] == 'S':
+            startStops.add(dic['stop_name'])
+            if dic['bus_id'] == 128:
+                stops[128]['start_stop'] = True
+            elif dic['bus_id'] == 256:
+                stops[256]['start_stop'] = True
+            else:
+                stops[512]['start_stop'] = True
+
+        # Checking final stops
+        elif dic['stop_type'] == 'F':
+            finishStops.add(dic['stop_name'])
+            if dic['bus_id'] == 128:
+                stops[128]['finish_stop'] = True
+            elif dic['bus_id'] == 256:
+                stops[256]['finish_stop'] = True
+            else:
+                stops[512]['finish_stop'] = True
+        currentBusLines.add(dic['bus_id'])
+
+    for bus_line, stops in stops.items():
+        if bus_line not in currentBusLines:
+            continue
+        for stopCheck in stops:
+            if not stops[stopCheck]:
+                print("There is no start or end stop for the line:", bus_line)
+                sys.exit()
+
+    # for key, value in transferStopsCounter.items():
+    #     print(key, value)
 
 
-def printErrorDictionary():
-    global lineNames
-    global numberOfStops
-    print("Line names and number of stops:")
-    for index in range(len(lineNames)):
-        print("bus_id:", lineNames[index], ", stops:", numberOfStops[index])
+def printStops():
+    global startStops, transferStops, finishStops
+    print("Start stops:", len(startStops), list(sorted(startStops)))
+    print("Transfer stops:", len(transferStops), list(sorted(transferStops)))
+    print("Finish stops:", len(finishStops), list(sorted(finishStops)))
 
 
 def startProgram():
     checkData(input_list)
-    printErrorDictionary()
+    printStops()
+
+
 
 
 startProgram()
