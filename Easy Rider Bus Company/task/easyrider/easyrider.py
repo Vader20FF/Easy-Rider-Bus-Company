593baf90018d6c411705dc5ad681c46042941fde
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-22 20:13:14 +0100

    Stage 5/6 of the project completed! - this stage of the project was about finding if every line has it's every stop's time arrival correct - the stop that are later in timetable should not have earlier bus arrival time

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -3,74 +3,40 @@ import sys
 
 json_input_string = input()
 input_list = json.loads(json_input_string)
-startStops = set()
-transferStops = set()
-finishStops = set()
+bus_lines_with_wrong_time = {}
+bus_lines_numbers_with_wrong_time = set()
 
 
 def checkData(json_string):
-    stops = {128: {'start_stop': False, 'finish_stop': False}, 256: {'start_stop': False, 'finish_stop': False},
-             512: {'start_stop': False, 'finish_stop': False}}
-    transferStopsCounter = {"Sesame Street": 0, "Fifth Avenue": 0, "Prospekt Avenue": 0, "Elm Street": 0,
-                            "Sunset Boulevard": 0, "Bourbon Street": 0, "Pilotow Street": 0, "Abbey Road": 0,
-                            "Santa Monica Boulevard": 0, "Beale Street": 0, "Startowa Street": 0, "Lombard Street": 0,
-                            "Orchard Road": 0, "Khao San Road": 0, "Michigan Avenue": 0, "Arlington Road": 0,
-                            "Niebajka Avenue": 0, "Jakis Street": 0, "Jakas Avenue": 0, "Karlikowska Avenue": 0,
-                            "Parizska Street": 0}
-    currentBusLines = set()
-    global startStops, transferStops, finishStops
+    global bus_lines_with_wrong_time
+    smallest_time = "00:00"
+    current_bus_id = 128
+
     for dic in json_string:
-        # Checking transfer stops
-        if dic['stop_name'] in transferStopsCounter:
-            transferStopsCounter[dic['stop_name']] += 1
-        if transferStopsCounter[dic['stop_name']] > 1:
-            transferStops.add(dic['stop_name'])
-
-        # Checking start stops
-        if dic['stop_type'] == 'S':
-            startStops.add(dic['stop_name'])
-            if dic['bus_id'] == 128:
-                stops[128]['start_stop'] = True
-            elif dic['bus_id'] == 256:
-                stops[256]['start_stop'] = True
-            else:
-                stops[512]['start_stop'] = True
-
-        # Checking final stops
-        elif dic['stop_type'] == 'F':
-            finishStops.add(dic['stop_name'])
-            if dic['bus_id'] == 128:
-                stops[128]['finish_stop'] = True
-            elif dic['bus_id'] == 256:
-                stops[256]['finish_stop'] = True
-            else:
-                stops[512]['finish_stop'] = True
-        currentBusLines.add(dic['bus_id'])
-
-    for bus_line, stops in stops.items():
-        if bus_line not in currentBusLines:
+        if dic['bus_id'] in bus_lines_numbers_with_wrong_time:
             continue
-        for stopCheck in stops:
-            if not stops[stopCheck]:
-                print("There is no start or end stop for the line:", bus_line)
-                sys.exit()
+        if dic['bus_id'] != current_bus_id:
+            smallest_time = "00:00"
+            current_bus_id = dic['bus_id']
+        if dic['a_time'] < smallest_time:
+            bus_lines_numbers_with_wrong_time.add(dic['bus_id'])
+            bus_lines_with_wrong_time[dic['bus_id']] = dic['stop_name']
+        smallest_time = dic['a_time']
 
-    # for key, value in transferStopsCounter.items():
-    #     print(key, value)
 
-
-def printStops():
-    global startStops, transferStops, finishStops
-    print("Start stops:", len(startStops), list(sorted(startStops)))
-    print("Transfer stops:", len(transferStops), list(sorted(transferStops)))
-    print("Finish stops:", len(finishStops), list(sorted(finishStops)))
+def printResult():
+    global bus_lines_with_wrong_time
+    print("Arrival time test:")
+    if bus_lines_with_wrong_time:
+        for bus_line, station in bus_lines_with_wrong_time.items():
+            print("bus_id line " + f'{bus_line}: wrong time on station {station}')
+    else:
+        print("OK")
 
 
 def startProgram():
     checkData(input_list)
-    printStops()
-
-
+    printResult()
 
 
 startProgram()
