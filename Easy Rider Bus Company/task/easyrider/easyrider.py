2d2bd9626fdbad336c2735549b71f8c9269f70a6
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-24 12:09:03 +0100

    Stage 6/6 of the project completed! - this stage of the project was about finding all stops which stop_type is on demand.

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -3,33 +3,22 @@ import sys
 
 json_input_string = input()
 input_list = json.loads(json_input_string)
-bus_lines_with_wrong_time = {}
-bus_lines_numbers_with_wrong_time = set()
+bus_stops_names_on_demand = []
 
 
 def checkData(json_string):
-    global bus_lines_with_wrong_time
-    smallest_time = "00:00"
-    current_bus_id = 128
+    global bus_stops_names_on_demand
 
     for dic in json_string:
-        if dic['bus_id'] in bus_lines_numbers_with_wrong_time:
-            continue
-        if dic['bus_id'] != current_bus_id:
-            smallest_time = "00:00"
-            current_bus_id = dic['bus_id']
-        if dic['a_time'] < smallest_time:
-            bus_lines_numbers_with_wrong_time.add(dic['bus_id'])
-            bus_lines_with_wrong_time[dic['bus_id']] = dic['stop_name']
-        smallest_time = dic['a_time']
+        if dic['stop_type'] == 'O' and dic['stop_name'] not in ['Fifth Avenue', 'Sesame Street', 'Bourbon Street',
+                                                                'Pilotow Street', 'Prospekt Avenue']:
+            bus_stops_names_on_demand.append(dic['stop_name'])
 
 
 def printResult():
-    global bus_lines_with_wrong_time
-    print("Arrival time test:")
-    if bus_lines_with_wrong_time:
-        for bus_line, station in bus_lines_with_wrong_time.items():
-            print("bus_id line " + f'{bus_line}: wrong time on station {station}')
+    print("On demand stops test:")
+    if bus_stops_names_on_demand:
+        print("Wrong stop type: ", sorted(bus_stops_names_on_demand))
     else:
         print("OK")
 
